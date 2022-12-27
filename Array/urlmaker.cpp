29337f83074bfdf15dfa38de9d7e7fdf83749900
Zahiruddin <zahir.khan9147@gmail.com>
2022-12-27 20:05:02 +0530

    URL MAker Added

---
+++ Array/urlmaker.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+string smallcharacter(string url)
+{
+    for (int i = 0; i < url.length(); i++)
+    {
+        if (url[i] >= 'A' && url[i] <= 'Z')
+        {
+            url[i] = url[i] + 32;
+        }
+    }
+    return url;
+}
+string addhyphon(string url)
+{
+    for (int i = 0; i < url.length(); i++)
+    {
+        if (url[i] == 32)
+        {
+            url[i] = '-';
+        }
+    }
+    return url;
+}
+
+int main()
+{
+    string str;
+    cout << "Enter url:";
+    getline(cin, str);
+    string ans = smallcharacter(str);
+    cout << "Normal Url:" << ans << endl;
+    string ans2 = addhyphon(ans);
+    cout << "URl with hyohon:" << ans2;
+
+    return 0;
+}
